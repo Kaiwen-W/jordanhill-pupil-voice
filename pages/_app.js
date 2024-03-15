a4d0c33952353b7d7bac1b374406fc785ba507fd
Kaiwen-W <111130181+Kaiwen-W@users.noreply.github.com>
2024-03-15 23:19:21 +0000

    added sign in page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { UserContext } from "@/lib/context";
+import { useUserData } from "@/lib/hooks";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Component {...pageProps} />;
+    </UserContext.Provider>
+  );
 }
