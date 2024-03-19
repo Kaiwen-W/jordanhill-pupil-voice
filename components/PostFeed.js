cd8ddde676e80decb23348a1429ba9c6dfac3133
Kaiwen-W <111130181+Kaiwen-W@users.noreply.github.com>
2024-03-19 23:25:50 +0000

    implemented user page logic (no UI)

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,24 +1,62 @@
-export default function PostFeed() {
-    return (
-        <div className="flex flex-col
-        m-0 h-full w-full 
-        overflow-hidden z-0 absolute left-10 items-center"> 
-            <PostItem /> 
-            <PostItem /> 
-        </div>  
-    )
+import Link from "next/link";
+
+export default function PostFeed({ posts, admin }) {
+  return posts
+    ? posts.map((post) => (
+        <PostItem post={post} key={post.slug} admin={admin} />
+      ))
+    : null;
 }
 
+function PostItem() {
+  // Calculates word count and approximate read time
+  const wordCount = post?.content.trim().split(/\s+/g).length;
+  const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
-function PostItem(){
-    return (
-    <div className="h-2/5 w-10/12
+  return (
+    <div
+      className="h-2/5 w-10/12
     bg-gray-800/30 border border-gray-900   
     mx-0 my-6 p-8 rounded-lg border-solid 
     shadow-md z-1
-    backdrop-blur-[100px]"> 
-        <h1 className="text-white text-3xl top-1/2 text-center mb-10"> Hello World! </h1>
-        <p className="text-white text-lg"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+    backdrop-blur-[100px]"
+    >
+      <Link>
+        <a href={`/${post.username}`}>
+          <strong> By @{post.username}</strong>
+        </a>
+      </Link>
+
+      <Link>
+        <h2>
+          <a href={`/${post.username}/${post.slug}`}>{post.title}</a>
+        </h2>
+      </Link>
+
+      <footer>
+        <span>
+          {wordCount} words. {minutesToRead} min read
+        </span>
+        <span className="">💗 {post.heartCount || 0} Hearts</span>{" "}
+        {/* push-left */}
+      </footer>
+
+      {/* If admin view, show extra controls for user */}
+      {admin && (
+        <>
+          <Link legacyBehavior href={`/admin/${post.slug}`}>
+            <h3>
+              <button className="bg-gray-500 text-white">Edit</button>
+            </h3>
+          </Link>
+
+          {post.published ? (
+            <p className="text-green-600">Live</p>
+          ) : (
+            <p className="text-red-600">Unpublished</p>
+          )}
+        </>
+      )}
     </div>
-    )
+  );
 }
--- components/SideBar.js
+++ components/SideBar.js
@@ -1,38 +1,70 @@
-import { MdSportsRugby } from "react-icons/md"; 
+import { MdSportsRugby } from "react-icons/md";
 import { CiMusicNote1 } from "react-icons/ci";
 import { IoMdSchool } from "react-icons/io";
 import { IoIosCreate } from "react-icons/io";
 import { MdAccountCircle } from "react-icons/md";
 import { FaPaintBrush } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
+import Link from "next/link";
+
+import { useContext } from "react";
+import { UserContext } from "@/lib/context";
 
 const SideBar = ({ house }) => {
+  const { user, username } = useContext(UserContext);
+
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 m-0 
-    flex flex-col bg-gray-900 text-white shadow z-0 items-center'>
-        <SideBarIcon icon={ <FaHome size="28" /> } text="Home" link="../" house={house}/>
-        <LineBreak />
+    <div
+      className="fixed top-0 left-0 h-screen w-16 m-0 
+    flex flex-col bg-gray-900 text-white shadow z-0 items-center"
+    >
+      <SideBarIcon
+        icon={<FaHome size="28" />}
+        text="Home"
+        link="../"
+        house={house}
+      />
+      <LineBreak />
 
-        <SideBarIcon icon={ <MdSportsRugby size="28" /> } text="Sports" house={house}/>
-        <SideBarIcon icon={ <CiMusicNote1 size="28"/> } text="Music" house={house}/> 
-        <SideBarIcon icon={ <IoMdSchool size="28"/> } text="Academia" house={house}/> 
-        <SideBarIcon icon={ <FaPaintBrush size="28"/> } text="Art" house={house}/> 
-        <LineBreak />
+      <SideBarIcon
+        icon={<MdSportsRugby size="28" />}
+        text="Sports"
+        house={house}
+      />
+      <SideBarIcon
+        icon={<CiMusicNote1 size="28" />}
+        text="Music"
+        house={house}
+      />
+      <SideBarIcon
+        icon={<IoMdSchool size="28" />}
+        text="Academia"
+        house={house}
+      />
+      <SideBarIcon icon={<FaPaintBrush size="28" />} text="Art" house={house} />
+      <LineBreak />
 
-        <SideBarIcon icon={ <IoIosCreate size="28"/> } text="Create" house={house}/> 
-        <SideBarIcon icon={ <MdAccountCircle size="28"/> } text="Account" house={house}/> 
+      <SideBarIcon
+        icon={<IoIosCreate size="28" />}
+        text="Create"
+        house={house}
+      />
+      <SideBarIcon
+        icon={<MdAccountCircle size="28" />}
+        text="Account"
+        house={house}
+        link={`/${username}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const SideBarIcon = ({ icon, text, link, house }) => (
   <a href={link}>
-    <div className={house}> 
+    <div className={house}>
       {icon}
 
-      <span className="sidebar-tooltip group-hover:scale-100"> 
-        {text}
-      </span>
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
   </a>
 );
@@ -40,7 +72,7 @@ const SideBarIcon = ({ icon, text, link, house }) => (
 const LineBreak = () => {
   return (
     <div className="bg-stone-300 h-[0.1px] w-12 opacity-30 mt-3 mb-3"></div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
