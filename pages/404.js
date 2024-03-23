59b116aa40162a5fe4f932c44184d057dce66319
Kaiwen-W <111130181+Kaiwen-W@users.noreply.github.com>
2024-03-23 17:31:09 +0000

    changed 404 image sign

--- pages/404.js
+++ pages/404.js
@@ -18,7 +18,7 @@ export default function Custom404() {
         <h1 className="text-white text-4xl p-8">
           UH OH! - That page does not exist...
         </h1>
-        <img src="/404.png" className="w-[45%] rounded-2xl" />
+        <img src="/404.png" className="w-[40%] rounded-2xl" />
         <Link href="/">
           <button className="mt-8 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 inline-flex items-center">
             Go home
