b33ba43ee1d670f4007958de5f86cf9e12a22ab8
Kaiwen-W <111130181+Kaiwen-W@users.noreply.github.com>
2024-04-03 22:50:06 +0100

    added an easter egg

--- pages/zeasteregg.js
+++ pages/zeasteregg.js
@@ -2,20 +2,20 @@ import toast from "react-hot-toast";
 
 const ForTestingOnly = () => {
   function success() {
-    toast.success("Well Done!");
+    toast.success("+ve!");
   }
 
   function error() {
-    toast.error("Boo!");
+    toast.error("-ve!");
   }
 
   return (
-    <div className="text-white mt-40">
+    <div className="text-white mt-40 flex items-center flex-col">
       <h1 className="text-5xl">A Page Dedicated to Testing New Features</h1>
-      <span>
-        If you found this by accident: I don't know how you did that. <br />
+      <span>If you found this by accident: I don't know how you did that.</span>
+      <p>
         If you found this through the source code: At least someone read it.
-      </span>
+      </p>
 
       <button className="text-3xl block" onClick={success}>
         Success Toast
